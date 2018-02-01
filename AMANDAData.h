a12a573cd225c8ffc6eee0559832e2c01698a3c8
Gabriele Gaetano Fronzé <gabriele.gaetano.fronze@cern.ch>
2018-02-01 12:30:16 +0100

    First commit for AMANDAData

---
+++ AMANDAData.h
@@ -0,0 +1,13 @@
+//
+// Created by Gabriele Gaetano Fronzé on 01/02/2018.
+//
+
+#ifndef ALICE_MTR_UTILS_AMANDADATA_H
+#define ALICE_MTR_UTILS_AMANDADATA_H
+
+class AMANDAData
+{
+
+};
+
+#endif //ALICE_MTR_UTILS_AMANDADATA_H
