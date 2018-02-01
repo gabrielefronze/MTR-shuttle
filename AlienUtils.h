5ab93338d56de8b97e0960008f7bee8a2eb94f1e
Gabriele Gaetano Fronzé <gabriele.gaetano.fronze@cern.ch>
2018-02-01 14:55:13 +0100

    Initial commit of AlienUtils

---
+++ AlienUtils.h
@@ -0,0 +1,13 @@
+//
+// Created by Gabriele Gaetano Fronzé on 01/02/2018.
+//
+
+#ifndef MTR_SHUTTLE_ALIENUTILS_H
+#define MTR_SHUTTLE_ALIENUTILS_H
+
+class AlienUtils
+{
+
+};
+
+#endif //MTR_SHUTTLE_ALIENUTILS_H
