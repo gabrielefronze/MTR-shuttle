4bcb6c4e10d6370a39bae631bbfa277dda891af0
Gabriele Gaetano Fronzé <gabriele.gaetano.fronze@cern.ch>
2018-02-05 18:37:31 +0100

    Removing duplicate parameter default value

--- RunObject.cxx
+++ RunObject.cxx
@@ -5,7 +5,7 @@
 #include "RunObject.h"
 
 RunObject::RunObject(uint64_t fSOR, uint64_t fEOR, double fAvgHV, double fAvgITot, double fAvgIDark, double fIntCharge,
-                     uint64_t fScalBending, uint64_t fScalNotBending, bool fIsDark=false) : fSOR(fSOR),
+                     uint64_t fScalBending, uint64_t fScalNotBending, bool fIsDark) : fSOR(fSOR),
                                                                               fEOR(fEOR),
                                                                               fAvgHV(fAvgHV),
                                                                               fAvgITot(fAvgITot),
