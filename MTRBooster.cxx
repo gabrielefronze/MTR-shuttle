909413488b07eb39181b368c24a54e4488c01db0
Gabriele Gaetano Fronzé <gabriele.gaetano.fronze@cern.ch>
2018-03-19 17:03:46 +0100

    Added interface class MTRBooster

---
+++ MTRBooster.cxx
@@ -0,0 +1,5 @@
+//
+// Created by Gabriele Gaetano Fronzé on 19/03/2018.
+//
+
+#include "MTRBooster.h"
---
+++ MTRBooster.h
@@ -0,0 +1,13 @@
+//
+// Created by Gabriele Gaetano Fronzé on 19/03/2018.
+//
+
+#ifndef MTR_SHUTTLE_MTRBOOSTER_H
+#define MTR_SHUTTLE_MTRBOOSTER_H
+
+class MTRBooster
+{
+
+};
+
+#endif //MTR_SHUTTLE_MTRBOOSTER_H
