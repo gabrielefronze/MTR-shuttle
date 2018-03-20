27274f9b99f94a50a8e6db014be942ff7c3420f3
Gabriele Gaetano Fronzé <gabriele.gaetano.fronze@cern.ch>
2018-03-20 17:31:19 +0100

    Addition of testBooster

---
+++ testBooster.c
@@ -0,0 +1,76 @@
+//
+// Created by Gabriele Gaetano Fronzé on 06/02/2018.
+//
+
+#include <Riostream.h>
+#ifdef APPLE
+#include <_types/_uint8_t.h>
+#include <_types/_uint16_t.h>
+#include <_types/_uint32_t.h>
+#include <_types/_uint64_t.h>
+#else
+#include <stdint.h>
+#endif
+#include <fstream>
+#include "MTRBooster.h"
+#include "RunObject.h"
+#include "TGraph.h"
+#include "TStyle.h"
+#include "TCanvas.h"
+#include "TLatex.h"
+#include "TLegend.h"
+#include "boost/any.hpp"
+#include "MTRConditions.h"
+
+void SetStyle(Bool_t graypalette) {
+  cout << "Setting style!" << endl;
+
+  gStyle->Reset("Plain");
+  gStyle->SetOptTitle(0);
+  gStyle->SetOptStat(0);
+  if(graypalette) gStyle->SetPalette(8,0);
+  else gStyle->SetPalette(1);
+  gStyle->SetCanvasColor(10);
+  gStyle->SetCanvasBorderMode(0);
+  gStyle->SetFrameLineWidth(1);
+  gStyle->SetFrameFillColor(kWhite);
+  gStyle->SetPadColor(10);
+  gStyle->SetPadTickX(1);
+  gStyle->SetPadTickY(1);
+  gStyle->SetPadBottomMargin(0.15);
+  gStyle->SetPadLeftMargin(0.15);
+  gStyle->SetHistLineWidth(1);
+  gStyle->SetHistLineColor(kRed);
+  gStyle->SetFuncWidth(2);
+  gStyle->SetFuncColor(kGreen);
+  gStyle->SetLineWidth(2);
+  gStyle->SetLabelSize(0.045,"xyz");
+  gStyle->SetLabelOffset(0.01,"y");
+  gStyle->SetLabelOffset(0.01,"x");
+  gStyle->SetLabelColor(kBlack,"xyz");
+  gStyle->SetTitleSize(0.05,"xyz");
+  gStyle->SetTitleOffset(1.25,"y");
+  gStyle->SetTitleOffset(1.2,"x");
+  gStyle->SetTitleFillColor(kWhite);
+  gStyle->SetTextSizePixels(26);
+  gStyle->SetTextFont(42);
+  //  gStyle->SetTickLength(0.04,"X");  gStyle->SetTickLength(0.04,"Y");
+
+  gStyle->SetLegendBorderSize(0);
+  gStyle->SetLegendFillColor(kWhite);
+  //  gStyle->SetFillColor(kWhite);
+  gStyle->SetLegendFont(42);
+
+
+}
+
+int testTemplate(){
+
+  SetStyle(false);
+
+  MTRBooster buster("MTR_2017_test90.csv");
+
+  buster.SetX("Time")->SetY("IntCharge")->SetRPC(5)->SetSide("IN")->SetPlane(4)->StackStage();
+
+  return 0;
+}
