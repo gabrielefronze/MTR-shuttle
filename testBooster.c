7914b885273ff4d848dd96baac564f314375ced2
Gabriele Gaetano Fronzé <gabriele.gaetano.fronze@cern.ch>
2018-03-22 18:10:45 +0100

    Update of testBooster

--- testBooster.c
+++ testBooster.c
@@ -66,7 +66,7 @@ void SetStyle(Bool_t graypalette) {
 
 int testBooster(){
 
-  SetStyle(false);
+//  SetStyle(false);
 
   MTRBooster buster("MTR_2017_test90.csv");
 
@@ -74,17 +74,55 @@ int testBooster(){
         .SetY("IntCharge")
         .AccumulateY()
         .OnlyIntegratedChargeRuns()
-//        .SetRPC(5)
-//        .SetSide("OUT")
+        .SetRPC(5)
+        .SetSide("OUT")
         .SetPlane(11)
         .StackStage();
 
-  TCanvas *canv=new TCanvas("canv","canv");
+  buster.SetX("Time").SetY("IDark").OnlyDarkCurrentRuns().SetPlane(11).SetSide("IN").StackStage();
+  buster.SetX("Time").SetY("IDark").OnlyDarkCurrentRuns().SetPlane(11).SetSide("OUT").StackStage();
 
-  auto mg = buster.Launch();
+  buster.SetX("Time").SetY("IDark").OnlyDarkCurrentRuns().SetPlane(12).SetSide("IN").StackStage();
+  buster.SetX("Time").SetY("IDark").OnlyDarkCurrentRuns().SetPlane(12).SetSide("OUT").StackStage();
 
-//  mg[0]->SetDirectory(0);
-  mg[0]->Draw("alp");
+  buster.SetX("Time").SetY("IDark").OnlyDarkCurrentRuns().SetPlane(21).SetSide("IN").StackStage();
+  buster.SetX("Time").SetY("IDark").OnlyDarkCurrentRuns().SetPlane(21).SetSide("OUT").StackStage();
+
+  buster.SetX("Time").SetY("IDark").OnlyDarkCurrentRuns().SetPlane(22).SetSide("IN").StackStage();
+  buster.SetX("Time").SetY("IDark").OnlyDarkCurrentRuns().SetPlane(22).SetSide("OUT").StackStage();
+
+  buster.Launch();
+
+  TCanvas *canv_single=new TCanvas("single_plot","single_plot");
+  buster.AutoDraw(0,canv_single->cd(),"alp");
+
+  TCanvas *canv_multiple=new TCanvas("multiple_plot","multiple_plot");
+  canv_multiple->Divide(2,2);
+
+  auto pad1 = canv_multiple->cd(1);
+  pad1->Divide(2,1);
+  buster.AutoDraw(1,pad1->cd(1),"alp",true);
+  buster.AutoDraw(2,pad1->cd(2),"alp",true);
+
+
+  auto pad2 = canv_multiple->cd(2);
+  pad2->Divide(2,1);
+  buster.AutoDraw(3,pad2->cd(1),"alp",true);
+  buster.AutoDraw(4,pad2->cd(2),"alp",true);
+
+
+  auto pad3 = canv_multiple->cd(3);
+  pad3->Divide(2,1);
+  buster.AutoDraw(5,pad3->cd(1),"alp",true);
+  buster.AutoDraw(6,pad3->cd(2),"alp",true);
+
+
+  auto pad4 = canv_multiple->cd(4);
+  pad4->Divide(2,1);
+  buster.AutoDraw(7,pad4->cd(1),"alp",true);
+  buster.AutoDraw(8,pad4->cd(2),"alp",true);
+
+
 
   return 0;
 }
