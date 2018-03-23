448eff9573fc48b5d2a1259873fd812a653a82ce
Gabriele Gaetano Fronzé <gabriele.gaetano.fronze@cern.ch>
2018-03-23 16:09:06 +0100

    Update of testBooster

--- testBooster.c
+++ testBooster.c
@@ -29,6 +29,13 @@ int testBooster(){
 
   buster.SetX("Time").SetY("IDark").OnlyDarkCurrentRuns().SetRPC(5).SetSide("OUT").SetPlane(11).StackStage();
 
+  buster.Launch();
+
+  TCanvas *canv_single=new TCanvas("single_plot","single_plot");
+  buster.AutoDraw(0,canv_single->cd());
+
+
+
   buster.SetX("Time").SetY("IntCharge").OnlyIntegratedChargeRuns().AccumulateY().PlotAverage().SetPlane(11).SetSide("IN").PlotMinMax().StackStage();
   buster.SetX("Time").SetY("IntCharge").OnlyIntegratedChargeRuns().AccumulateY().PlotAverage().SetPlane(11).SetSide("OUT").PlotMinMax().StackStage();
 
@@ -43,9 +50,6 @@ int testBooster(){
 
   buster.Launch();
 
-  TCanvas *canv_single=new TCanvas("single_plot","single_plot");
-  buster.AutoDraw(0,canv_single->cd());
-
   TCanvas *canv_multiple=new TCanvas("multiple_plot","multiple_plot");
   canv_multiple->Divide(2,2);
 
@@ -54,18 +58,15 @@ int testBooster(){
   buster.AutoDraw(1,pad1->cd(1),true);
   buster.AutoDraw(2,pad1->cd(2),true);
 
-
   auto pad2 = canv_multiple->cd(2);
   pad2->Divide(2,1);
   buster.AutoDraw(3,pad2->cd(1),true);
   buster.AutoDraw(4,pad2->cd(2),true);
 
-
   auto pad3 = canv_multiple->cd(3);
   pad3->Divide(2,1);
   buster.AutoDraw(5,pad3->cd(1),true);
   buster.AutoDraw(6,pad3->cd(2),true);
-
 
   auto pad4 = canv_multiple->cd(4);
   pad4->Divide(2,1);
