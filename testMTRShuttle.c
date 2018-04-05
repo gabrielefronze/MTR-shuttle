26582e3580d90751e1cb47f63e9efffd5c781cb1
Gabriele Gaetano Fronzé <gabriele.gaetano.fronze@cern.ch>
2018-04-05 21:17:52 +0200

    testMTRShuttle for Max crosschecks

--- testMTRShuttle.c
+++ testMTRShuttle.c
@@ -9,14 +9,18 @@ void testMTRShuttle(){
   MTRShuttle sciattol;
 
 //  sciattol.parseRunList("../runlists_Max/allruns_2016-2017.txt");
-  sciattol.parseRunList("../runs_2017_test90");
+//  sciattol.parseRunList("../runs_2017_test90");
+  sciattol.parseRunList("../crosscheck_Max/runlist_calib.txt");
 //  sciattol.parseRunList("runsTestPropagation");
   sciattol.parseOCDB("local:///Users/Gabriele/cernbox/Dottorato/MTR2017/CDB/####/OCDB");
   sciattol.parseAMANDAiMon("../IMon2017_UTF8.txt");
   sciattol.propagateAMANDA();
+  sciattol.computeAverage();
+
   for( const auto &it : sciattol.fRunDataVectAvg[4]){
     std::cout << it.isDark();
   }
   std::cout << std::endl;
-  sciattol.saveData("MTR_2017_test90.csv");
+
+  sciattol.saveData("../crosscheck_Max/test_calib.csv");
 }
